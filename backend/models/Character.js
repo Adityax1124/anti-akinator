7aba92ab10feca478e6a8df65da8968cd18142ba
Adityax1124 <adisinghx11@gmail.com>
2026-07-01 08:25:28 +0530

    Fix: Update backend dependencies and nixpacks config

--- backend/models/Character.js
+++ backend/models/Character.js
@@ -18,7 +18,7 @@ const characterSchema = new mongoose.Schema({
   },
   description: {
     type: String,
-    required: true,
+    required: false,
     maxlength: 6000
   },
     crucialHint: {                    
